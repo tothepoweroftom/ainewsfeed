529809c8fc5435fc95d5a8e510dc95a4fd9b9052
Tom Power <tothepoweroftom@gmail.com>
2022-08-09 11:25:30 +0200

    looks good

--- main.py
+++ main.py
@@ -11,12 +11,18 @@ from newsapi import NewsApiClient
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
+from firebase_admin import db
+
 import datetime
 
 
 cred = credentials.Certificate('firebase.json')
 firebase_admin.initialize_app(cred, {
-    'storageBucket': 'second-flame-357709.appspot.com'
+    'storageBucket': 'second-flame-357709.appspot.com', 
+    'databaseURL': 'https://second-flame-357709-default-rtdb.europe-west1.firebasedatabase.app/',
+     'databaseAuthVariableOverride': {
+        'uid': 'my-service-worker'
+    }
 })
 
 load_dotenv()
@@ -39,7 +45,7 @@ if not os.path.exists('images'):
 temp_dir = "./images"
 
 
-def getPicture(ind):
+def getPicture():
     date = datetime.date.today().strftime("%B %d, %Y")
     top_headlines = newsapi.get_top_headlines(
         sources="bbc-news",
@@ -47,54 +53,59 @@ def getPicture(ind):
     )
     titles = []
     count = 0
-    headline = top_headlines["articles"][ind]
+    headline = random.choice(top_headlines["articles"])
     print(headline["title"])
     titles.append(headline["title"])
-    styles = ["A Soviet propoganda poster of ", "A beautiful painting by William Turner of ", "A vivid pencil sketch of ", "An abstract art representation of ", "A beautiful painting by Van Gogh of ", "A generative digital artwork of " "A watercolour sketch of ", "A colourful Japanese etching of ", "A celtic artwork of", "A geometric artwork of ", "A minimalistic pointillism painting of ", "A paper collage artwork of "]        
-    prompt = random.choice(styles) + random.choice(titles)  + ", " + "trending on Artstation"
+    
+    styles = ["A Soviet propoganda poster of ",  "A beautiful painting of ", "A vivid pencil sketch of ", "An abstract art representation of ", "A beautiful painting of ", "A generative digital artwork of " "A watercolour sketch of ", "A colourful Japanese etching of ", "A celtic artwork of ", "A geometric artwork of ", "A minimalistic pointillism painting of ", "A paper collage artwork of "]        
+    artists = ["Hiroshi Yoshida", "Max Ernst", "Paul Signac", "Salvador Dali", "James Gurney", "M.C. Escher", "Thomas Kinkade", "Ivan Aivazovsky", "Italo Calvino", "Norman Rockwell", "Albert Bierstadt", "Giorgio de Chirico", "Rene Magritte", "Ross Tran", "Marc Simonetti", "John Harris", "Hilma af Klint", "George Inness", "Pablo Picasso", "William Blake", "Wassily Kandinsky", "Peter Mohrbacher", "Greg Rutkowski", "Paul Signac", "Steven Belledin", "Studio Ghibli"]
+    prompt = random.choice(styles) + random.choice(titles)  + " by " + random.choice(artists) +  ", " + "trending on Artstation"
     print(prompt)
     prediction_generator = replicate.models.get("nightmareai/disco-diffusion").predict(
         prompt=prompt,
-        steps="40",
+        steps="100",
     )
+    print(prediction_generator)
  
     for index, url in enumerate(prediction_generator):
         print(url)
         # construct filename
-        prefix = str(ind).zfill(4)  # 0001, 0002, etc.
         uuid = url.split("/")[-2]
         extension = url.split(".")[-1]  # jpg, png, etc
         filename = f"{temp_dir}/{uuid}.{extension}"
 
         # download and save the file
-        if(index > 0):
+        if(index == 6):  
+            print("Getting image from url ---")
             data = requests.get(url)
             image_data = data.content
 
             bucket = storage.bucket()
             blob = bucket.blob(f"{date}/{uuid}.{extension}")
+            print("Uploading to Google Storage")
             blob.upload_from_string(
                     image_data,
                     content_type=f"image/{extension}"
-                )
+            )
+
             # Opt : if you want to make public access from the URL
             blob.make_public()
             print("your file url", blob.public_url)
+            ref = db.reference('artworks/'+ f"{date}/{uuid}")
+            ref.set({'imageURL':blob.public_url, 'headline': headline["title"], 'prompt': prompt})
+            return
+          
           
 
 
 @app.route("/")
 def hello_world():
-    
-   
-
-
+    getPicture()
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
 
 
 if __name__ == "__main__":
-    getPicture(3)
-    # app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 # [END run_helloworld_service]
 # [END cloudrun_helloworld_service]
